d6ffc624ee82c8a5a246782a0e3b78295db19600
Palina Cetin <fpe@tut.by>
2020-11-16 17:31:54 +0300

    feat: make field sizes

--- gem-puzzle/src/Field.js
+++ gem-puzzle/src/Field.js
@@ -2,8 +2,8 @@
 import Gem from './Gem.js';
 
 export default class PuzzleField {
-  constructor(fieldSize) {
-    this.fieldSize = fieldSize;
+  constructor() {
+    this.fieldSize = null;
     this.gameStart = null;
     this.time = '00 : 00';
     this.timer = null;   
@@ -12,12 +12,19 @@ export default class PuzzleField {
     this.field = null;
     this.animation = false;
 
-    this.gem = {width:100, height:100, speed:0.2};
+    this.gem = {};
     this.lw = 20; //толщина рамки поля
-  }
+    this.canvas = {width: 440, height: 440};
+
+    this.sounds = {chip: new Audio('./assets/sounds/chip.mp3'),
+                   over: new Audio('./assets/sounds/level_completed.mp3'),};
+    this.isSound = true;
+  } 
 
   generate() {
-    this._generateGemsArray();        
+    this.applySettings(4);
+    this._generateGemsArray();
+    console.log(this.gems);        
 
     this.field = document.querySelector('#CANVAS');    
     //this.field.width = this.fieldSize*this.gem.width;
@@ -28,26 +35,66 @@ export default class PuzzleField {
         let hole = this._checkEmptyPlace(gem);        
         if (hole && !this.animation) {
           this._startAnimation(gem, hole);
+          if (this.isSound) this._soundPlay(this.sounds.chip);          
         }
       }
     });    
     
-    this._renderField();    
+    this._renderField();
+    
+    const buttonCont = document.createElement('div');     // button-container  
+    buttonCont.classList.add('button-container');
+    const exitButton = document.createElement('button');
+    exitButton.classList.add('exit');
+    exitButton.textContent = 'EXIT';
+    buttonCont.append(exitButton);    
+    
+    const soundButton = document.createElement('button');
+    soundButton.classList.add('sound');    
+    soundButton.innerHTML = `<i class="material-icons">music_note</i>`;
+    buttonCont.append(soundButton);
 
+    soundButton.addEventListener('click', () => {
+      this._toggleSound();
+      soundButton.innerHTML = this.isSound ? `<i class="material-icons">music_note</i>` : `<i class="material-icons">music_off</i>`;
+    });
+
+    document.body.prepend(buttonCont);
+
+
+    const cont = document.createElement('div');     // time-container  
+    cont.classList.add('time-container');
     const movesLabel = document.createElement('div');
     movesLabel.classList.add('moves');
     movesLabel.textContent = `Moves: ${this.moves}`;
-    document.body.prepend(movesLabel);
+    cont.append(movesLabel);
     
     const timeLabel = document.createElement('div');
     timeLabel.classList.add('time');    
-    document.body.prepend(timeLabel);
+    cont.append(timeLabel);
     timeLabel.textContent = `Time: ${this.time}`;
 
+    document.body.prepend(cont);    
+    
+  }  
+
+  applySettings(size=4) {
+    this.fieldSize = size;
+    this.gem.width = (this.canvas.width - 2*this.lw)/this.fieldSize;
+    this.gem.height = (this.canvas.height - 2*this.lw)/this.fieldSize;
+    this.gem.speed = 0.2;
+    console.log(this.gem, this.fieldSize);
+  }
+
+  startNewGame() {
+    this.gems = [];
+    this._generateGemsArray();
+    console.log(this.gems);
+    this._renderField();
     this.gameStart = new Date().getTime();
     this.timer = setInterval(this._updateTime.bind(this), 1000);
-    
   }
+
 
   _updateTime() {
     const now = new Date().getTime();
@@ -201,8 +248,11 @@ export default class PuzzleField {
     const ctx = this.field.getContext('2d');        
 
     let gradient1 = ctx.createLinearGradient(0, 0, 500, 500);
-    gradient1.addColorStop('0', 'black');    
-    gradient1.addColorStop('1', 'blue');    
+    gradient1.addColorStop('0', '#f37055');    
+    gradient1.addColorStop('0.25', '#ef4e7b');    
+    gradient1.addColorStop('0.5', '#5073b8');    
+    gradient1.addColorStop('0.75', '#1098ad');    
+    gradient1.addColorStop('1', '#6fba82');    
 
     ctx.fillStyle = '#e2e0e9';          
     ctx.strokeStyle = gradient1;
@@ -213,7 +263,7 @@ export default class PuzzleField {
     this.gems.forEach(elem => {
       if (elem.number !== 0) { //Gem с номером 0, будет пустым местом
         const sw = 2;
-        const r = 37;
+        const r = 0.37*this.gem.width;
         ctx.fillStyle = '#9a8ceb';          
         ctx.strokeStyle = '#c0b3ec';
         ctx.lineWidth = sw;      
@@ -227,12 +277,21 @@ export default class PuzzleField {
 
         ctx.fillStyle = '#4e4d53';
         ctx.textBaseline = 'middle';
-        ctx.font = 'bold 42px serif';
+        ctx.font = `bold ${0.42*this.gem.width}px serif`;
         ctx.textAlign = 'center';
         ctx.fillText(elem.number, (elem.x + 0.5)*this.gem.width+this.lw+sw/2, (elem.y + 0.5)*this.gem.height+this.lw+sw/2);      
       }   
     });     
   }
+
+  _soundPlay = function(sound) {
+    sound.currentTime=0;
+    sound.play();
+  };
+
+  _toggleSound() {
+    this.isSound = !this.isSound;
+  };
 
   
 
---
+++ gem-puzzle/src/settings.js
@@ -0,0 +1,20 @@
+'use strict';
+
+export default function createSettings() {
+  const cont = document.createElement('div');
+  cont.classList.add('settings-page');
+  cont.innerHTML = `<h2>Settings</h2>
+        <p>You may choose field size:</p>
+        <select id="fieldSize">
+          <option value="3">3x3</option>
+          <option value="4" selected>4x4</option>
+          <option value="5">5x5</option>
+          <option value="6">6x6</option>
+          <option value="7">7x7</option>
+          <option value="8">8x8</option>
+        </select>
+        <p id="information"></p>
+        <button class = "back settings_back"><i class="material-icons">arrow_back</i></button>`;
+    
+  return cont;  
+}
